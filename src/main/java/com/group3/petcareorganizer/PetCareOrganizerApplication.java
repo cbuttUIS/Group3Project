8023e4af275665b51c64b1253017efb54e8249c1
Chelsea Button <cbutt@uis.edu>
2025-11-15 12:42:50 +0000

    Add spring boot structure.

---
+++ src/main/java/com/group3/petcareorganizer/PetCareOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.group3.petcareorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCareOrganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group3/petcareorganizer/PetCareOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.petcareorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
