d021b8425b21fb6da11ca625a3e7c34b83c910d3
Chelsea Button <cbutt@uis.edu>
2025-11-19 19:49:26 +0000

    Commit changes for spring boot structure to main

--- src/main/java/com/group3/petcareorganizer/PetCareOrganizerApplication.java
+++ src/main/java/com/group3/petcareorganizer/PetCareOrganizerApplication.java
@@ -1,13 +1,25 @@
 package com.group3.petcareorganizer;
 
+import com.group3.petcareorganizer.service.UserService;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class PetCareOrganizerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetCareOrganizerApplication.class, args);
-    }
 
+
+        var context = SpringApplication.run(PetCareOrganizerApplication.class, args);
+
+
+        UserService userService = context.getBean(UserService.class);
+
+        int factorialOf5 = userService.factorial(5);
+        System.out.println("Factorial of 5 is: " + factorialOf5);
+    }
 }
--- src/main/java/com/group3/petcareorganizer/service/UserService.java
+++ src/main/java/com/group3/petcareorganizer/service/UserService.java
@@ -1,4 +1,22 @@
-package main.java.com.group3.petcareorganizer.service;
+package com.group3.petcareorganizer.service;
+import org.springframework.stereotype.Service;
 
+
+// Annotation
+@Service
+
+
+// Class
 public class UserService {
+
+    // Method to compute factorial
+    public int factorial(int n){
+
+        //base case
+        if(n==0){
+            return 1;
+        }
+        return n * factorial(n-1);
+
+    }
 }
